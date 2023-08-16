2f9fb88e88581dca05fd14a46ebb9f814b711637
ashraf56 <ashrafulfahim07@gmail.com>
2023-08-16 16:07:21 +0600

    initial design of  login and sign up page

--- app/login/page.js
+++ app/login/page.js
@@ -1,20 +1,66 @@
 'use-client'
+import Image from 'next/image';
 import React from 'react';
-
+import img from '../asset/login.png'
+import Link from 'next/link';
 const page = () => {
     return (
-        <div className='flex flex-col items-center justify-center min-h-screen w-full text-center '>
-  <div className='flex  shadow-xl w-5/6 card  '>
-    <div className='w-3/6'>
-    <h2>imag section</h2>
+        <div >
+    <div className="hero min-h-screen bg-base-200">
+  <div className="hero-content flex-col lg:flex-row card text-white  ">
+  <div className='w-3/6 '>
+    <Image  src={img} alt='login' width={700}/>
    </div>
+    <div className=' w-full md:w-3/6 bg-gradient-to-r from-[#ef709b] to-[#fa9372] card-body  rounded-e-lg '>
+      <h1 className="text-2xl text-center font-bold py-5">Log in Now</h1>
+<div className='justify-center'>
+   <form >
+<div className='form-control'>
+<label className='py-2 text-xs md:text-sm' >Email</label>
+<input type="email" placeholder="Type here" className="input input-bordered w-full max-w-xl lg:max-w-md" />
+</div>
+<div className='form-control'>
+<label className='py-2 text-xs md:text-sm' >Password</label>
+<input type="password" placeholder="Type here" className="input input-bordered  w-full max-w-xl lg:max-w-md" />
+</div>
+<div className='form-control'>
+<label className='py-2 text-xs md:text-sm' >
+ <Link href={'/signup'}>
+ Create New Account
+ </Link> 
+  </label>
 
-   <div className='w-3/6 bg-orange-400'>
-<h2>form element</h2>
+</div>
 
- </div>
-    </div>        
-   
+
+<div className='text-center py-2'>
+  <button className='btn btn-neutral  md:w-full '>
+    Login  
+  </button>
+</div>
+
+     </form>
+     <div className=" divider  text-sm">OR LogIn with </div>
+<div className=' flex items-center justify-center py-1 '>
+  <button className='btn btn-circle btn-outline btn-primary mx-2'>
+G  
+</button>
+  <button className='btn btn-circle btn-outline btn-primary mx-2'>
+    in
+  </button>
+  <button className='btn btn-circle btn-outline mx-2 btn-primary '>
+   F
+  </button>
+</div>
+
+
+</div>
+    
+
+    </div>
+  </div>
+</div>
+
         </div>
     );
 };
