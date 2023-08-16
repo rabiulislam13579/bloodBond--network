9fb8d80e046d2b37e1969015db0dc404ed3e5b24
sagar-ghosh1 <sagorghosh520@gmail.com>
2023-08-16 11:00:44 +0600

    new brance created

--- app/page.js
+++ app/page.js
@@ -2,13 +2,14 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-   
-   <>
-   <h1>Blood Donating Website</h1>
-   <h1>bloodBond website</h1>
-   <h2>heloo there</h2>
-   <h2>heloo there2</h2>
-   <h2>heloo there3</h2>
-   </>
+
+    <>
+      <h1>Blood Donating Website</h1>
+      <h1>bloodBond website</h1>
+      <h2>heloo there</h2>
+      <h2>heloo there2</h2>
+      <h2>heloo there3</h2>
+      <p>Hello there4</p>
+    </>
   )
 }
