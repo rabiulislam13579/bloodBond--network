aa01e0299fa2895d43a860d670004bc5d2ac9bf3
ashraf56 <ashrafulfahim07@gmail.com>
2023-08-16 12:08:26 +0600

    tailwind css code set in the global css

---
+++ app/login/page.js
@@ -0,0 +1,22 @@
+'use-client'
+import React from 'react';
+
+const page = () => {
+    return (
+        <div className='flex flex-col items-center justify-center min-h-screen w-full text-center '>
+  <div className='flex  shadow-xl w-5/6 card  '>
+    <div className='w-3/6'>
+    <h2>imag section</h2>
+   </div>
+
+   <div className='w-3/6 bg-orange-400'>
+<h2>form element</h2>
+
+ </div>
+    </div>        
+   
+        </div>
+    );
+};
+
+export default page;
