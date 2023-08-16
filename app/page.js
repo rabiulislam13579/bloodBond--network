6aa34947e6c5783fb972452ae70c68bdfeaff30d
sagar-ghosh1 <sagorghosh520@gmail.com>
2023-08-16 11:28:07 +0600

    second part pitter

--- app/page.js
+++ app/page.js
@@ -1,15 +1,11 @@
+import Works from '@/components/works/works'
 import Image from 'next/image'
 
 export default function Home() {
   return (
 
     <>
-      <h1>Blood Donating Website</h1>
-      <h1>bloodBond website</h1>
-      <h2>heloo there</h2>
-      <h2>heloo there2</h2>
-      <h2>heloo there3</h2>
-      <p>Hello there4</p>
+      <Works></Works>
     </>
   )
 }
---
+++ components/works/works.js
@@ -0,0 +1,11 @@
+
+const Works = () => {
+    return (
+        <div>
+            <h1>The pitter</h1>
+            <h1>The pitter</h1>
+        </div>
+    );
+};
+
+export default Works;
