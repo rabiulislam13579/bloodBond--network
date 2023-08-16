4f9c1adc154e5751dd87ddbc6ba07868e96f90c9
Istaik Ahamad <ahmedruhanbmarpc@gmail.com>
2023-08-16 12:43:07 +0600

    header section for bloodbond-network

--- app/page.js
+++ app/page.js
@@ -1,13 +1,24 @@
 import Image from 'next/image'
-
-export default function Home() {
+import { FcSearch } from "react-icons/fc";
+export const metadata = {
+  title: 'BloodBond--Network || Home',
+  description: 'Spider Alpha',
+}
+ const HomePage=()=> {
   return (
    
    <>
-   <h1>Blood Donating Website</h1>
-   <h1>bloodBond website</h1>
-   <h2> there</h2>
+  <div className='w-[25%] mx-auto flex gap-2'>
+    <div>
+    
+   <input  type="text" placeholder="Search Your Location" className="input input-bordered w-full max-w-xs" />
+
+    </div>
+    <button  className="btn btn-outline btn-error" >Search  <FcSearch className=' text-3xl'></FcSearch></button>
+   
+  </div>
    
    </>
   )
 }
+export default HomePage;
