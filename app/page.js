0c53ca718c4fbead528a8f7932c63eb94a40b8c0
Istaik Ahamad <ahmedruhanbmarpc@gmail.com>
2023-08-17 12:05:27 +0600

    about section started

--- app/page.js
+++ app/page.js
@@ -1,18 +1,24 @@
 "use client"
 import { FcSearch } from "react-icons/fc";
-import { useRef, useState
- } from "react";
+
+import AwesomeSlider from 'react-awesome-slider';
+import withAutoplay from 'react-awesome-slider/dist/autoplay';
+import 'react-awesome-slider/dist/styles.css';
+import 'react-awesome-slider/dist/custom-animations/cube-animation.css';
+import React, { useRef, useState } from 'react';
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
 
 // Import Swiper styles
 import 'swiper/css';
-import 'swiper/css/effect-coverflow';
-import 'swiper/css/pagination';
+import 'swiper/css/effect-cards';
+
 
 
 // import required modules
-import { EffectCoverflow, Pagination } from 'swiper/modules';
+import { EffectCards } from 'swiper/modules';
+const AutoplaySlider = withAutoplay(AwesomeSlider);
+
 
 
 
@@ -34,48 +40,37 @@ export const metadata = {
    
   </div>
 
-  <div className="pt-16">
-  <Swiper
-        effect={'coverflow'}
-        grabCursor={true}
-        centeredSlides={true}
-        slidesPerView={'auto'}
-        coverflowEffect={{
-          rotate: 50,
-          stretch: 0,
-          depth: 100,
-          modifier: 1,
-          slideShadows: true,
-        }}
-        pagination={true}
-        modules={[EffectCoverflow, Pagination]}
-        className="mySwiper"
-      >
-        <SwiperSlide >
-          <img src="https://i.ibb.co/9ZvZr5r/pic-3.jpg" alt=""
-        
-          ></img>
-        </SwiperSlide>
 
-         <SwiperSlide>
-         <img src="https://i.ibb.co/GV7bvZ7/pic-4.jpg" alt=""
-           
-           ></img>
-         </SwiperSlide>
-         <SwiperSlide>
-         <img src="https://i.ibb.co/zr85Nds/pic-2.jpg" alt=""
+{/* banner section */}
+
+
+  <section className="w-[90%] mx-auto pt-12">
+  <AutoplaySlider
+             animation="cubeAnimation"
+    play={true}
+    cancelOnInteraction={false} // should stop playing on user interaction
+    interval={6000}
+  >
+    <div className="rounded-md" data-src="https://i.ibb.co/rkbNpFV/pic-5.jpg" />
+    <div className="rounded-md" data-src="https://i.ibb.co/9ZvZr5r/pic-3.jpg"  />
+    <div className="rounded-md" data-src="https://i.ibb.co/GV7bvZ7/pic-4.jpg" />
+    <div className="rounded-md" data-src="https://i.ibb.co/zr85Nds/pic-2.jpg" />
+    <div className="rounded-md" data-src="https://i.ibb.co/zm0HPrZ/pic-1.jpg" />
+  </AutoplaySlider>
+      </section>
+
+{/* about section */}
+
+
+      <section className="flex">
+        <div>
+      
+        </div>
+        <div>
           
-          ></img>
-         </SwiperSlide>
-         
-        <SwiperSlide>
-        <img src="https://i.ibb.co/zm0HPrZ/pic-1.jpg"  alt=""
-            
-            ></img>
-        </SwiperSlide>
-       
-      </Swiper>
-      </div>
+        <h1 className="text-5xl text-center font-bold text-red-500 pt-12">About Us</h1>
+        </div>
+      </section>
    
    </>
   )
