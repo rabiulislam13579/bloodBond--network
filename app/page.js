30c0ecb5f4d5c61695344be608341fc52fbaa675
Istaik Ahamad <ahmedruhanbmarpc@gmail.com>
2023-08-17 21:36:07 +0600

    completed the about section

--- app/page.js
+++ app/page.js
@@ -1,78 +1,106 @@
-"use client"
+"use client";
 import { FcSearch } from "react-icons/fc";
 
-import AwesomeSlider from 'react-awesome-slider';
-import withAutoplay from 'react-awesome-slider/dist/autoplay';
-import 'react-awesome-slider/dist/styles.css';
-import 'react-awesome-slider/dist/custom-animations/cube-animation.css';
-import React, { useRef, useState } from 'react';
+import AwesomeSlider from "react-awesome-slider";
+import withAutoplay from "react-awesome-slider/dist/autoplay";
+import "react-awesome-slider/dist/styles.css";
+import "react-awesome-slider/dist/custom-animations/cube-animation.css";
+import React, { useRef, useState } from "react";
 // Import Swiper React components
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/effect-cards';
-
-
+import "swiper/css";
+import "swiper/css/effect-cards";
 
 // import required modules
-import { EffectCards } from 'swiper/modules';
+import { EffectCards } from "swiper/modules";
 const AutoplaySlider = withAutoplay(AwesomeSlider);
 
-
-
-
 export const metadata = {
-  title: 'BloodBond--Network || Home',
-  description: 'Spider Alpha',
-}
- const HomePage=()=> {
+  title: "BloodBond--Network || Home",
+  description: "Spider Alpha",
+};
+const HomePage = () => {
   return (
-   
-   <>
-  <div className='w-[25%] mx-auto flex gap-2'>
-    <div>
-    
-   <input  type="text" placeholder="Search Your Location" className="input input-bordered w-full max-w-xs" />
+    <>
+      <div className="w-[25%] mx-auto flex gap-2">
+        <div>
+          <input
+            type="text"
+            placeholder="Search Your Location"
+            className="input input-bordered w-full max-w-xs"
+          />
+        </div>
+        <button className="btn btn-outline btn-error">
+          Search <FcSearch className=" text-3xl"></FcSearch>
+        </button>
+      </div>
 
-    </div>
-    <button  className="btn btn-outline btn-error" >Search  <FcSearch className=' text-3xl'></FcSearch></button>
-   
-  </div>
+      {/* banner section */}
 
-
-{/* banner section */}
-
-
-  <section className="w-[90%] mx-auto pt-12">
-  <AutoplaySlider
-             animation="cubeAnimation"
-    play={true}
-    cancelOnInteraction={false} // should stop playing on user interaction
-    interval={6000}
-  >
-    <div className="rounded-md" data-src="https://i.ibb.co/rkbNpFV/pic-5.jpg" />
-    <div className="rounded-md" data-src="https://i.ibb.co/9ZvZr5r/pic-3.jpg"  />
-    <div className="rounded-md" data-src="https://i.ibb.co/GV7bvZ7/pic-4.jpg" />
-    <div className="rounded-md" data-src="https://i.ibb.co/zr85Nds/pic-2.jpg" />
-    <div className="rounded-md" data-src="https://i.ibb.co/zm0HPrZ/pic-1.jpg" />
-  </AutoplaySlider>
+      <section className="w-[90%] mx-auto pt-12">
+        <AutoplaySlider
+          animation="cubeAnimation"
+          play={true}
+          cancelOnInteraction={false} // should stop playing on user interaction
+          interval={6000}
+        >
+          <div
+            className="rounded-md"
+            data-src="https://i.ibb.co/rkbNpFV/pic-5.jpg"
+          />
+          <div
+            className="rounded-md"
+            data-src="https://i.ibb.co/9ZvZr5r/pic-3.jpg"
+          />
+          <div
+            className="rounded-md"
+            data-src="https://i.ibb.co/GV7bvZ7/pic-4.jpg"
+          />
+          <div
+            className="rounded-md"
+            data-src="https://i.ibb.co/zr85Nds/pic-2.jpg"
+          />
+          <div
+            className="rounded-md"
+            data-src="https://i.ibb.co/zm0HPrZ/pic-1.jpg"
+          />
+        </AutoplaySlider>
       </section>
 
-{/* about section */}
+      {/* about section */}
 
-
-      <section className="flex">
+      <section className="mt-16 w-[90%] grid grid-cols-2 gap-5 mx-auto">
+        
+          <div className="avatar">
+            <div className="w-auto rounded">
+              <img src="https://i.ibb.co/3RRXjvG/about-1.jpg" />
+            </div>
+          </div>
+        
         <div>
-      
-        </div>
-        <div>
-          
-        <h1 className="text-5xl text-center font-bold text-red-500 pt-12">About Us</h1>
+          <h1 className="text-5xl text-center font-bold text-red-500 pt-12">
+            About Us
+          </h1>
+          <p className="text-gray-500 pt-10">
+            Safe blood saves lives. Blood is needed by women with complications
+            during pregnancy and childbirth, children with severe anaemia, often
+            resulting from malaria or malnutrition, accident victims and
+            surgical and cancer patients. There is a constant need for a regular
+            supply of blood because it can be stored only for a limited period
+            of time before use. Regular blood donation by a sufficient number of
+            healthy people is needed to ensure that blood will always be
+            available whenever and wherever it is needed. Blood is the most
+            precious gift that anyone can give to another person – the gift of
+            life. A decision to donate your blood can save a life, or even
+            several if your blood is separated into its components – red cells,
+            platelets and plasma – which can be used individually for patients
+            with specific conditions. <button className="btn btn-outline btn-error btn-xs">see more</button>
+          </p>
         </div>
       </section>
-   
-   </>
-  )
-}
+    </>
+  );
+};
 export default HomePage;
