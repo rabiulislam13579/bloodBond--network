3f7b8f9bafa2104ac903ac5de03a577866e2395a
Istaik Ahamad <ahmedruhanbmarpc@gmail.com>
2023-08-18 11:54:33 +0600

    check

--- app/login/page.js
+++ app/login/page.js
@@ -20,7 +20,7 @@ const page = () => {
 <input type="email" placeholder="Type here" className="input input-bordered w-full max-w-xl lg:max-w-md" />
 </div>
 <div className='form-control'>
-<label className='py-2 text-xs md:text-sm' >Password</label>
+<label className='py-2 text-xs md:text-sm' >Password c</label>
 <input type="password" placeholder="Type here" className="input input-bordered  w-full max-w-xl lg:max-w-md" />
 </div>
 <div className='form-control'>
