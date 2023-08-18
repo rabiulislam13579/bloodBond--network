76d3b76d6db2c98d780228f1699a0cc94d1d35ea
farjanarahman <mahfuzurrahman.dev@gmail.com>
2023-08-18 16:10:20 +0600

    nav and footer done

--- app/page.js
+++ app/page.js
@@ -1,11 +1,9 @@
-import WorksUs from '@/src/components/WorksUs/WorksUs'
 import Image from 'next/image'
 
 export default function Home() {
   return (
 
     <>
-      <WorksUs></WorksUs>
       <h1>hello world 4</h1>
       <h1>hello world 4</h1>
       <h1>hello world 4</h1>
