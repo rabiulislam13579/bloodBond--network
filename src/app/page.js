90ee7f7ba50c3710c0da8859ea253e6c542f47f4
sagar-ghosh1 <sagorghosh520@gmail.com>
2023-08-18 18:00:19 +0600

    my first works Us section

--- src/app/page.js
+++ src/app/page.js
@@ -3,22 +3,17 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-<<<<<<< HEAD:app/page.js
 
     <>
       <WorksUs></WorksUs>
+      <h1>hello world 4</h1>
+      <h1>hello world 4</h1>
+      <h1>hello world 4</h1>
+      <h1>hello world 4</h1>
+      <h1>hello world 4</h1>
+      <h2>Hiiiii</h2>
+      <h2>Hiiiii</h2>
     </>
-=======
-   
-   <>
-<h1>hello world 4</h1>
-<h1>hello world 4</h1>
-<h1>hello world 4</h1>
-<h1>hello world 4</h1>
-<h1>hello world 4</h1>
-<h2>Hiiiii</h2>
-<h2>Hiiiii</h2>
-   </>
->>>>>>> fd252192aa0934fdbf32f13350085f8859cd9757:src/app/page.js
+
   )
 }
